8a6e988ad4dc2c76daf1c4fd7ed55fc90147c074
Seth Reno <sethreno@gmail.com>
2025-01-04 10:45:22 -0600

    Add line for page break in png

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ pushd output
 mkdir -p temp
 pdftoppm -png resume.pdf > temp/page1.png
 pdftoppm -png resume.pdf -f 2 > temp/page2.png
-convert temp/page1.png -gravity South -chop 0x150 temp/page1.png
 convert temp/page2.png -gravity North -chop 0x100 temp/page2.png
 convert temp/page2.png -gravity South -chop 0x100 temp/page2.png
 convert temp/page1.png temp/page2.png -append resume.png
+convert resume.png -strokewidth 1 -stroke gray -draw "line 0,1600 1300,1600" resume.png
 popd
